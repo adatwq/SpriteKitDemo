224b56ca0ec0983d89d038ec60b410ae715dde3e
AzizShalhoub <a.shalhoub88@gmail.com>
2024-03-10 11:16:31 +0300

    Initial Commit

---
+++ SpriteKitDemo/SpriteKitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpriteKitDemoApp.swift
+//  SpriteKitDemo
+//
+//  Created by Aziz Dev on 10/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct SpriteKitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
